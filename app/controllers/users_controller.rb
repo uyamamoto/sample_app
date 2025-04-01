71140b8a2002c4c3aac30faa5c29beee85d4f582
Yuki Yamamoto <yuki120908yg@icloud.com>
2025-04-01 20:52:02 +0900

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,22 +8,22 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      reset_session
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "please check your email to activate you account."
+      redirect_to root_url
     else
       render 'new', status: :unprocessable_entity
     end
   end
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def edit
